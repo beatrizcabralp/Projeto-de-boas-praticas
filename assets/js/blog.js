2e7f70c00df2ceb19f43c76a6f5de1654453f704
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2025-06-07 20:33:58 -0300

    criar post ok
    já é possível criar postagens

---
+++ assets/js/blog.js
@@ -0,0 +1,17 @@
+console.log('Ola a todos!');
+
+const postagem = document.querySelector('form');
+console.log(postagem);
+
+
+postagem.addEventListener('submit', function criaPostController(InfosdoEvento) {
+    InfosdoEvento.preventDefault();
+    console.log('Estamos criando um post novo!')
+    const criapost = document.querySelector('textarea[name="criapost"]');
+    const listaDePublicacoes = document.querySelector('#lista-de-publicacoes');
+
+    listaDePublicacoes.insertAdjacentHTML('afterbegin', `<li>${criapost.value}</li>`)
+    criapost.value='';
+
+
+})
