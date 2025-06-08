0c89df93f10a9db62d456c866fc4a56ef38bc08f
Ana Beatriz Cabral Pinto <beatrizcabralp.2002@gmail.com>
2025-06-07 22:50:30 -0300

    ler e escrever postagens feito

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,16 +1,45 @@
-console.log('Ola a todos!');
+const blog = {
+    usuario:[
+        {
+          username: 'beatriz',
+        }
+    ],
+    postagens:[
+        {
+            id: 1,
+            owner: 'beatriz',
+            content: 'meu primeiro post'
+        }
+    ],
+    criarpostagem(dados, html0nly = false) {
+        if(!html0nly){
+            blog.postagens.push({
+                id: blog.postagens.length + 1,
+                owner: dados.owner,
+                content: dados.content
+            })
+        }
+        
+
+        const listaDePublicacoes = document.querySelector('#lista-de-publicacoes');
+        listaDePublicacoes.insertAdjacentHTML('afterbegin', `<li id="publicacao">${dados.content}</li>`)
+    }
+}
 
 const postagem = document.querySelector('form');
 console.log(postagem);
 
+blog.postagens.forEach(({owner, content}) => {
+    blog.criarpostagem({owner: owner, content: content}, true);
+})
 
 postagem.addEventListener('submit', function criaPostController(InfosdoEvento) {
     InfosdoEvento.preventDefault();
     console.log('Estamos criando um post novo!')
     const criapost = document.querySelector('textarea[name="criapost"]');
-    const listaDePublicacoes = document.querySelector('#lista-de-publicacoes');
 
-    listaDePublicacoes.insertAdjacentHTML('afterbegin', `<li id="publicacao">${criapost.value}</li>`)
+    blog.criarpostagem({owner:'beatriz', content: criapost.value});
+   
     criapost.value='';
 
 
